9a2aca8420fde474afd6b8192339c2588b53464c
Killian <killian.vaillant@gmail.com>
2022-11-30 14:21:32 +0100

    Add stripe

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,10 @@ Rails.application.routes.draw do
   resources :profils 
   
   resources :products
+
+  scope '/checkout' do
+    post 'create', to: 'checkout#create', as: 'checkout_create'
+    get 'success', to: 'checkout#success', as: 'checkout_success'
+    get 'cancel', to: 'checkout#cancel', as: 'checkout_cancel'
+end
 end
